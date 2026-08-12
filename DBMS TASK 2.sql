be7f0e6497382e25c878e44f8bf1ed80d2ac6985
angeljanee <angel1738janee@gmail.com>
2026-08-12 14:33:58 +0530

    Add files via upload

---
+++ DBMS TASK 2.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE InventoryDB;
+USE InventoryDB;
+
+ CREATE TABLE Category (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(100) UNIQUE NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE Product (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(150) NOT NULL,
+    category_id INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT 0 NOT NULL,
+    CONSTRAINT fk_product_category
+        FOREIGN KEY (category_id)
+        REFERENCES Category(category_id)
+);
+
+INSERT INTO Category (category_name, description)
+VALUES
+('Electronics', 'Electronic devices and accessories'),
+('Clothing', 'Men and women clothing'),
+('Books', 'Educational and general books'),
+('Furniture', 'Home and office furniture'),
+('Sports', 'Sports equipment and accessories');
+
+SELECT * FROM Category;
+
+INSERT INTO Product (product_name, category_id, price, stock)
+VALUES
+('Laptop', 1, 55000.00, 10),
+('Smartphone', 1, 25000.00, 20),
+('T-Shirt', 2, 799.00, 30),
+('Jeans', 2, 1499.00, 15),
+('Java Programming Book', 3, 599.00, 25),
+('Study Table', 4, 4500.00, 8),
+('Cricket Bat', 5, 2500.00, 12);
+
+SELECT * FROM Product;
+
